ff59a2325b0411bc07748a6d187ff993600746b9
nguyenvietthi <nguyenvietthi.nd@gmail.com>
2023-10-04 23:12:36 +0700

    commit code

--- insert_tmr.py
+++ insert_tmr.py
@@ -3,12 +3,41 @@ filename = r"./test.sv"
 inst_voter = "dti_voter voter_inst (.in0(b_0), .in1(b_1), .in2(b_2), out(b));"
 
 def find_q_port(text):
-  result = re.search(r'.Q\((.*?)\)', text)
-  print(result)
-  if(result):
-    return result.group(1).replace(" ", "")
+  pattern = re.compile(r'\.Q\((.*?)\)')
+  matches = pattern.findall(text)
+
+  max_values = {}
+  for item in matches:
+      match = re.match(r'^([^\[]+)(?:\[(\d+)\])?$', item)
+      if match:
+          key, value = match.groups()
+          if value is not None:
+              value = int(value)
+              if key not in max_values or value > max_values[key][0]:
+                  max_values[key] = (value, item)
+          else:
+              max_values[item] = (None, item)
+              print(max_values)
+
+  unique_data = []
+  for key in max_values:
+    if(max_values[key][0] == None):
+        unique_data.append(max_values[key][1])
+    else:
+       unique_data.append(max_values[key][1][:-1] + ":0]")
+
+
+  if(len(matches)):
+    return matches[0], unique_data
   else:
-    return None
+    return None, None
+
+def first_word(text):
+    match = re.match(r'\b\w+\b', text)
+    if match:
+        return match.group()
+    else:
+        return None
 
 def instances_info(text):
   return text.split(" ")[0:2]
@@ -22,7 +51,6 @@ def add_suffix(text, i):
 
 f = open("test.sv", "r")
 verilog_content = f.read()
-# print(verilog_content)
 
 verilog_content = re.sub(r'\s+', ' ', verilog_content)
 verilog_content = verilog_content.replace("\n", " ").replace("; ", ";\n").replace("endmodule ", "endmodule\n").replace("( ", "(").replace(" )", ")");
@@ -30,48 +58,41 @@ port_split = verilog_content.split("\n")
 
 module_split = verilog_content.split("endmodule")
 
-print((module_split[1]))
-print(find_q_port(module_split[1]))
-# print(port_split)
 
+f = open("out.sv", "w")
+voter_index = 0
+module_index = -1 
+for module in module_split:
+  mark_voter = 0
+  q_port_list = find_q_port(module)[-1]
+  print(q_port_list)
+  for line in module.split("\n"):
+    q_port = find_q_port(line)[0]
+    if(q_port):
+      for i in range(3):
+        ins_info = instances_info(line)
+        f.write(line
+          .replace(".Q(" + q_port, ".Q(" + add_suffix(q_port, i))
+          .replace(ins_info[1], add_suffix(ins_info[1], i)) + "\n")
+      f.write(inst_voter
+        .replace("b_0", add_suffix(q_port, 0))
+        .replace("b_1", add_suffix(q_port, 1)).replace("b_2", add_suffix(q_port, 2))
+        .replace("b", q_port)
+        .replace("voter_inst", "voter_" + str(voter_index)) + "\n")
+      voter_index = voter_index + 1
 
-# f = open("out.sv", "w")
-# voter_index = 0;
-# module_index = -1 
-# for line in port_split:
+    elif (first_word(line) == "wire"):
+      try:
+        f.write(line + "\n")
+        if(mark_voter == 0):
+          f.write("wire " + ", ".join([add_suffix(port, 0) for port in q_port_list]) 
+                  + ", " +", ".join([add_suffix(port, 1) for port in q_port_list])
+                  + ", " +", ".join([add_suffix(port, 2) for port in q_port_list])
+                  + ";\n")
+          mark_voter = 1
+      except:
+        f.write(line + "\n")
+    else:
+      f.write(line + "\n")
 
-#   if(len(re.findall(r'module', line))):
-#     voter_index = 0;
-#     module_index = module_index + 1
-#     try:
-#       # print(module_split[module_index])
-#       q_port_in_module = find_q_port(module_split[module_index])
-#       # print(q_port_in_module)
-#       for port in q_port_in_module:
-#         # print(port)
-#         if(port):
-#           f.write(line 
-#             + "\n" + "wire " + add_suffix(port, 0) + ";"
-#             + "\n" + "wire " + add_suffix(port, 1) + ";"
-#             + "\n" + "wire " + add_suffix(port, 2) + ";")
-#     except:
-#       pass
-
-#   q_port = find_q_port(line)
-#   if(q_port):
-#     for i in range(3):
-#       ins_info = instances_info(line)
-#       f.write(line
-#         .replace(".Q(" + q_port, ".Q(" + add_suffix(q_port, i))
-#         .replace(ins_info[1], add_suffix(ins_info[1], i)) + "\n")
-#     f.write(inst_voter
-#       .replace("b_0", add_suffix(q_port, 0))
-#       .replace("b_1", add_suffix(q_port, 1)).replace("b_2", add_suffix(q_port, 2))
-#       .replace("b", q_port)
-#       .replace("voter_inst", "voter_" + str(voter_index)) + "\n")
-#     voter_index = voter_index + 1
-
-#   else:
-#     f.write(line + "\n")
-
-# f.close()
+f.close()
